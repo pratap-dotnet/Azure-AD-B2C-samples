1899beb91e4557b6d3a1e088ff9bf5432fe92cb9
Pratap Bhaskar <Pratap.Bhaskar@harman.com>
2016-10-13 18:51:51 +0530

    minor changes

--- SecondWebApplication/Controllers/AccountController.cs
+++ SecondWebApplication/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using Microsoft.Owin.Security;
 
@@ -16,7 +18,21 @@ namespace SecondWebApplication.Controllers
             if (!Request.IsAuthenticated)
             {
                 HttpContext.GetOwinContext().Authentication.Challenge(
-                    new AuthenticationProperties { RedirectUri = "/" }, Startup.SignInPolicyId);
+                    new AuthenticationProperties { RedirectUri = Startup.redirectUri }, Startup.SignInPolicyId);
+            }else
+            {
+                Response.Redirect("/");
+            }
+        }
+
+
+        public void LogOff()
+        {
+            if (Request.IsAuthenticated)
+            {
+                IEnumerable<AuthenticationDescription> authTypes = HttpContext.GetOwinContext().Authentication.GetAuthenticationTypes();
+                HttpContext.GetOwinContext().Authentication.SignOut(authTypes.Select(t => t.AuthenticationType).ToArray());
+                Request.GetOwinContext().Authentication.GetAuthenticationTypes();
             }
         }
     }
